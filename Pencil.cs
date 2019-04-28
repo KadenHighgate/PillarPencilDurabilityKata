444576663f259543bfd122f94b0e30cd3b78c81b
Kaden Highgate <kaden.highgate@gmail.com>
2019-04-28 17:10:34 -0400

    Initial commit

---
+++ Pencil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillarPencilDurabilityKata
+{
+    public class Pencil
+    {
+        internal void Write(Paper paper, string content)
+        {
+            paper.content = content;
+        }
+    }
+}
---
+++ Tests/PencilTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PillarPencilDurabilityKata.Tests
+{
+    [TestFixture]
+    class PencilTests
+    {
+
+
+
+        [Test]
+        [Category("pass")]
+        public void whenWeWriteAStringThatStringIsOnPaperAccurately()
+        {
+            Pencil pencil = new Pencil();
+            Paper paper = new Paper();
+            pencil.Write(paper, "Writing");
+
+            Assert.AreEqual("Writing", paper.content);
+        }
+    }
+}
